670ad1dbef2ddd7ddf4447723fb998181daf40de
David Keogh <davekeogh@shaw.ca>
2008-09-05 00:12:17 -0700

    Moved the Application class to it's own module.

--- main.py
+++ main.py
@@ -7,42 +7,9 @@ from window import Window
 from worker import Worker
 from archive import Archive
 from error import ArchiveError
+from application import Application
 
 GLADE = 'colorwalk.glade'
-
-
-class Application(object):
-	
-	# TODO: This class is only going to become larger, better to move it
-	#		to its own module.
-	
-	# TODO: Many of these properties would probably make more sense to
-	#		reside in the Archive object
-	
-	win = None
-	worker = None
-	archive = None
-	log = ['\n']
-	files = []
-	images = []
-	current = 0
-	size = 0
-	
-	scale = 0
-	
-	current_pb = None
-	next_pb = None
-	previous_pb = None
-	
-	def reset(self):
-		self.files = []
-		self.images = []
-		self.current = 0
-		self.size = 0
-	
-		self.current_pb = None
-		self.next_pb = None
-		self.previous_pb = None
 
 
 def main(args):
