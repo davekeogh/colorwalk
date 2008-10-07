7f1f8ac9c021cc23067b80bfbd45f9f250e747a1
David Keogh <davekeogh@shaw.ca>
2008-10-06 20:18:15 -0700

    Added some properties to the BookmarkError class.

--- error.py
+++ error.py
@@ -1,8 +1,10 @@
 
 class BookmarkError(Exception):
 	
-	def __init__(self, message):
-		self.message = message
+	def __init__(self, file, old_page, new_page):
+		self.file = file
+		self.old_page = old_page
+		self.new_page = new_page
 
 
 class ArchiveError(Exception):
