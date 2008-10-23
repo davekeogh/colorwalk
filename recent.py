3b0e38e67576a51976935a1d3c9b67f0bc122e6c
David Keogh <davekeogh@shaw.ca>
2008-10-23 14:01:36 -0700

    A list-like object to store the recent file names.

---
+++ recent.py
@@ -0,0 +1,43 @@
+import os, os.path
+
+import gtk
+
+from preferences import PREFS_DIR
+
+
+class RecentFiles(object):
+    
+    file = os.path.join(PREFS_DIR, 'recent')
+    files = []
+    
+    def __init__(self):
+        if os.path.isfile(self.file):
+            fb = open(self.file)
+            self.files = fb.read().strip('\n').split('\n')
+            fb.close()
+    
+    
+    def add(self, file):
+        if len(self.files) <= 9:
+            self.files.append(file)
+        else:
+            self.files = self.files[1:]
+            self.files.append(file)
+    
+    
+    def read(self):
+        return self.files
+    
+    
+    def save(self):
+        if len(self.files):
+            fb = open(self.file, 'w')
+            
+            for file in self.files:
+                fb.write('%s\n' % file)
+            
+            fb.close()
+    
+    
+    def __del__(self):
+        self.save()
