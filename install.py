528ccdac9527c38de20714a702978b9a7f2c3810
David Keogh <davekeogh@shaw.ca>
2009-04-04 21:12:21 -0700

    Make the install script a bit easier to extend.

--- install.py
+++ install.py
@@ -5,6 +5,15 @@ import sys, os, subprocess, optparse
 VERSION = '0.1.0'
 PREFIX = '/usr'
 
+def install(file, location):
+    print 'Copying %s to %s/%s ...' % (file, PREFIX, location) ,
+    if subprocess.call(['install', '%s' % file, os.path.join(PREFIX, os.path.join(location, file))]):
+        print 'Failed'
+        print '\nColor Walk failed to install.'
+        sys.exit(1)
+    else:
+        print 'Done'
+
 parser = optparse.OptionParser()
 parser.add_option('--prefix', dest='prefix', help='define an alternate location to install to')
 (options, args) = parser.parse_args()
@@ -16,28 +25,8 @@ print 'Installing Color Walk %s :\n' % VERSION
 
 subprocess.call(['mkdir', '-p', os.path.join(PREFIX, 'share/colorwalk')])
 
-print 'Copying colorwalk.ui to %s/share/colorwalk ...' % PREFIX ,
-if subprocess.call(['install', 'colorwalk.ui', os.path.join(PREFIX, 'share/colorwalk/colorwalk.ui')]):
-    print 'Failed'
-    print '\nColor Walk failed to install.'
-    sys.exit(1)
-else:
-    print 'Done'
-
-print 'Copying colorwalk.desktop to %s/share/applications ...' % PREFIX ,
-if subprocess.call(['install', 'colorwalk.desktop', os.path.join(PREFIX, 'share/applications/colorwalk.desktop')]):
-    print 'Failed'
-    print '\nColor Walk failed to install.'
-    sys.exit(1)
-else:
-    print 'Done'
-
-print 'Copying colorwalk to %s/bin ...' % PREFIX ,
-if subprocess.call(['install', 'colorwalk', os.path.join(PREFIX, 'bin/colorwalk')]):
-    print 'Failed'
-    print '\nColor Walk failed to install.'
-    sys.exit(1)
-else:
-    print 'Done'
+install('colorwalk.ui', 'share/colorwalk')
+install('colorwalk.desktop', 'share/applications')
+install('colorwalk', 'bin')
 
 print '\nColor Walk %s is now installed.' % VERSION
